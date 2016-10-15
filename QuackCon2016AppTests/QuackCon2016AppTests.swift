fc4d241a7e3e06115509e26e0def354ab29ca413
Daniel Seitz <dnseitz@gmail.com>
2016-10-15 01:32:05 -0700

    Initial Commit

---
+++ QuackCon2016AppTests/QuackCon2016AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  QuackCon2016AppTests.swift
+//  QuackCon2016AppTests
+//
+//  Created by Daniel Seitz on 10/15/16.
+//  Copyright © 2016 Daniel Seitz. All rights reserved.
+//
+
+import XCTest
+@testable import QuackCon2016App
+
+class QuackCon2016AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
